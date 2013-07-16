fb66df254b02c21706e673f1c25782d2243cfe52
Coleman Foley <colemanfoley@gmail.com>
2013-07-16 14:43:08 -0700

    Made a function for changing view when the desired image changes.

--- code.js
+++ code.js
@@ -1,11 +1,15 @@
-var currentImg = 1;
 $(document).ready(function(){
-	$('#imageSet li').first().show();
-
 	$('#buttonSet li').click(function(){
-		$('#imageSet li').hide();
-		$('#imageSet li:nth-child(' + ($(this).index()+ 1) + ')').show();
-		$('#buttonSet li').css('background-color', 'white');
-		$(this).css('background-color', 'black');
-	})
-})
+		handleImageChange($(this));
+	});
+
+	handleImageChange($('#buttonSet li').first());
+});
+
+var handleImageChange = function(thisObject) {
+	$('#imageSet li').hide();
+	$('#imageSet li:nth-child(' + ($(thisObject).index() + 1) + ')').show();
+
+	$('#buttonSet li').css('background-color', 'white');
+	$(thisObject).css('background-color', 'black');
+};
